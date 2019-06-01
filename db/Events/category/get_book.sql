8e6b68ad8a386e89102f93d2540c0c5da61ff653
Kevin Ung <ung.kevin78@gmail.com>
2019-06-01 14:26:48 -0600

    group categorizing

--- db/Events/category/get_book.sql
+++ db/Events/category/get_book.sql
@@ -1,4 +1,5 @@
-select events.event_id, event_name, day, month, date, year, start_time, end_time, image, address, city ,state, zipcode, details
+select events.event_id, event_name, day, month, events.date, year, start_time, end_time, image, address, city ,state, zipcode, details, groups.date as dating, family,friends,large
 from events 
 join category on category.event_id = events.event_id
+join groups on groups.event_id = events.event_id
 where book = ${category} 
--- db/Events/get_all_events.sql
+++ db/Events/get_all_events.sql
@@ -1,2 +1,3 @@
-select * 
+select events.event_id, event_name, day, month, events.date, year, start_time, end_time, image, address, city ,state, zipcode, details, groups.date as dating, family,friends,large
 from events
+join groups on groups.event_id = events.event_id
